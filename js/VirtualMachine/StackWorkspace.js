af52487f2de33b6c1afc66d208321cea1a04c5dc
Dmitriy Gruzdev <Dmitriy_Gruzdev@epam.com>
2016-05-03 15:39:47 +0300

    Stack added

---
+++ js/VirtualMachine/StackWorkspace.js
@@ -0,0 +1,36 @@
+function StackWorkspace(maxSize) {
+    var self = this;
+    var MAX_STACK_SIZE = maxSize;
+    var stackPointer = 0;
+    var stack = [];
+
+    return {
+        getStackPointer: getStackPointer,
+        pop: pop,
+        push: push
+    };
+
+    function pop() {
+        if (vm_stack_pointer > 0) {
+            return stack[--stackPointer];
+        }
+        else {
+            //TODO: errorHandler STACK_EMPTY
+            return 0;
+        }
+    }
+
+    function push(word) {
+        if (stackPointer < MAX_STACK_SIZE) {
+            stack[stackPointer++] = word;
+        }
+        else {
+            //TODO: errorHandler STACK_OVERFLOW
+        }
+    }
+
+    function getStackPointer() {
+        return stackPointer;
+    }
+
+}
--- js/VirtualMachine/VirtualMachine.js
+++ js/VirtualMachine/VirtualMachine.js
@@ -1,3 +1,4 @@
 function VirtualMachine() {
-
+    var programMemory = new ProgramMemory(65536);
+    var dataMemory = new DataMemory(65536);
 }
