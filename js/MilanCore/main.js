47e8598d018822dd0f6297ab0673d924464dec4e
ginvaell <ginvaell@gmail.com>
2016-05-03 22:52:47 +0300

    some

--- js/MilanCore/main.js
+++ js/MilanCore/main.js
@@ -6,29 +6,49 @@ var hello = 'Hello';
 var scanner = new Scanner(' +  begin end');
 
 var command = new Command(Opcodes.get().LOAD, 15);
-console.dir(command);
 
-var str = command.getStr(47);
-console.log(str);
+var ce = new CodeEmitter('hello\n');
+
+// ce.emit(Opcodes.get().PUSH, 25);
+// ce.emit(Opcodes.get().LOAD, 13);
+// ce.makeHole();
+// ce.emit(Opcodes.get().STOP);
+// ce.emitAt(1, Opcodes.get().ADD);
+// console.log(ce.flush());
+
+// scanner = new Scanner("/* Greatest common divisor */" +
+//     "BEGIN\n " +
+//     "a := READ;" +
+//     "b := READ;" +
+//     "WHILE a != b DO " +
+//     "IF a < b THEN " +
+//     "b := b - a " +
+//     "ELSE " +
+//     "a := a - b " +
+//     "FI " +
+//     "OD ;" +
+//     "WRITE(a) " +
+//     "END " +
+//     "$");
+
+// while (scanner.token !== Tokens.get().EOF) {
+//     scanner.nextToken();
+//     console.log(scanner.token);
+//     console.log(scanner.value);
+// }
 
 scanner = new Scanner("/* Greatest common divisor */" +
     "BEGIN\n " +
     "a := READ;" +
     "b := READ;" +
-    "WHILE a != b DO " +
-    "IF a < b THEN " +
     "b := b - a " +
-    "ELSE " +
     "a := a - b " +
-    "FI " +
-    "OD ;" +
-    "WRITE(a) " +
     "END " +
     "$");
 
-while (scanner.token !== Tokens.get().EOF) {
-    scanner.nextToken();
-    console.log(scanner.token);
-    console.log(scanner.value);
-}
+var parser = new Parser(scanner, '');
+console.log(parser.parse());
+console.dir(parser.getVarTable());
+
+
 
