531a18a0d9f63d38ccb062d3ffe63d460d92c803
Dmitriy Gruzdev <Dmitriy_Gruzdev@epam.com>
2016-05-03 17:46:37 +0300

    RuntimeErrors style fix

--- js/VirtualMachine/RuntimeErrors.js
+++ js/VirtualMachine/RuntimeErrors.js
@@ -2,20 +2,20 @@
 
 var RuntimeErrors = (function () {
     function RuntimeErrors(type, message) {
-        this.type = name;
-        this.message = code;
+        this.type = type;
+        this.message = message;
     }
 
     var instance = {
         BAD_DATA_ADDRESS: new RuntimeErrors('BAD_DATA_ADDRESS', "Error: illegal data address"),
         BAD_CODE_ADDRESS: new RuntimeErrors('BAD_CODE_ADDRESS', "Error: illegal address in JUMP* instruction"),
-        BAD_RELATION: new RuntimeErrors('BAD_RELATION', "Error: illegal comparison operator"),
-        STACK_OVERFLOW: new RuntimeErrors('STACK_OVERFLOW', "Error: stack overflow"),
-        STACK_EMPTY: new RuntimeErrors('STACK_EMPTY', "Error: stack is empty (no arguments are available)"),
+        BAD_RELATION:     new RuntimeErrors('BAD_RELATION',     "Error: illegal comparison operator"),
+        STACK_OVERFLOW:   new RuntimeErrors('STACK_OVERFLOW',   "Error: stack overflow"),
+        STACK_EMPTY:      new RuntimeErrors('STACK_EMPTY',      "Error: stack is empty (no arguments are available)"),
         DIVISION_BY_ZERO: new RuntimeErrors('DIVISION_BY_ZERO', "Error: division by zero"),
-        BAD_INPUT: new RuntimeErrors('BAD_INPUT', "Error: illegal input"),
-        UNKNOWN_COMMAND: new RuntimeErrors('UNKNOWN_COMMAND', "Error: unknown command, unable to execute"),
-        DEFAULT: new RuntimeErrors('DEFAULT', "Error: runtime error"),
+        BAD_INPUT:        new RuntimeErrors('BAD_INPUT',        "Error: illegal input"),
+        UNKNOWN_COMMAND:  new RuntimeErrors('UNKNOWN_COMMAND',  "Error: unknown command, unable to execute"),
+        DEFAULT:          new RuntimeErrors('DEFAULT',          "Error: runtime error")
     };
 
     function get() {
